00a4a6247125b7e8b4018333b36476d282a4a2af
Enrique Millan Valbuena <enrique.millanvalbuena@gmail.com>
2021-06-26 19:45:09 +0200

    ENH, CLN: Add tabulate and black formatting

--- pnl.py
+++ pnl.py
@@ -2,89 +2,124 @@ import csv
 import sys
 from collections import defaultdict, deque
 
+import pandas as pd
+from tabulate import tabulate
+
 # ASSUMPTIONS MADE
 # only one trade for a particular symbol at one time tick
-# csv always sorted by time (instructions kind of say this, unclear) 
+# csv always sorted by time (instructions kind of say this, unclear)
 #   necessary or else my usage of deque is not right
 # can't trade for fractions of cent
 # csv always right: price: 2 degree of precision float, quantity: int
 
-class TradeManager():
-    def __init__(self, store_trades=True, print_trades=False):
-        self._open_trades = defaultdict(deque)
-        self._closed_trades = []
-        self._store_trades = store_trades
-        self._print_trades = print_trades
+
+class TradeManager:
+    def __init__(self):
+
+        self.open_trades = defaultdict(deque)
+        self.closed_trades = []
         self._pnl = 0.0
 
     def process_trade(self, trade):
-        d = self._open_trades[trade.symbol]
+        """FIFO trade processing.
 
-        # if no inventory, just add it
-        if len(d) == 0:
-            d.append(trade)
+        Parameters
+        ----------
+        trade : [type]
+            [description]
+        """
+        open_trades = self.open_trades[trade.symbol]
+
+        # If no inventory, just add it
+        if len(open_trades) == 0:
+            open_trades.append(trade)
             return
 
-        # if inventory exists, all trades must be same way (buy or sell)
-        # if new trade is same way, again just add it
-        if d[0].buying == trade.buying:
-            d.append(trade)
+        # If inventory exists, all trades must be same way (buy or sell)
+        # If new trade is same way, again just add it
+        if open_trades[0].buying == trade.buying:
+            open_trades.append(trade)
             return
 
-        # otherwise, consume the trades
-        while len(d) > 0 and trade.quantity > 0:
-            quant_traded = min(trade.quantity, d[0].quantity)
-            
-            pnl = quant_traded * round(trade.price - d[0].price, 2)
-            # invert if we shorted
+        # Otherwise, consume the trades
+        while len(open_trades) > 0 and trade.quantity > 0:
+
+            quant_traded = min(trade.quantity, open_trades[0].quantity)
+
+            pnl = quant_traded * round(trade.price - open_trades[0].price, 2)
+
+            # Invert if we shorted
             if trade.buying:
                 pnl *= -1
 
             pnl = round(pnl, 2)
             self._pnl += pnl
 
-            ct = ClosedTrade(d[0].time, trade.time, trade.symbol,
-                    quant_traded, pnl, d[0].buying, d[0].price, trade.price)
+            # Create closed trade
+            ct = ClosedTrade(
+                open_trades[0].time,
+                trade.time,
+                trade.symbol,
+                quant_traded,
+                pnl,
+                open_trades[0].buying,
+                open_trades[0].price,
+                trade.price,
+            )
 
-            if self._print_trades:
-                print(ct)
-            elif self._store_trades:
-                self._closed_trades.append(ct)
+            self.closed_trades.append(ct)
 
+            # Remove closed shares from the trade
             trade.quantity -= quant_traded
-            d[0].quantity -= quant_traded
-            
-            if d[0].quantity == 0:
-                d.popleft()
+            open_trades[0].quantity -= quant_traded
+
+            if open_trades[0].quantity == 0:
+                open_trades.popleft()
 
         # if the new trade still has quantity left over
         # then add it
         if trade.quantity > 0:
-            d.append(trade)
-            
-    def process_csv(self, file_name):
-        with open(file_name, 'r') as trades_csv:
-            trade_reader = csv.DictReader(trades_csv, delimiter=',')
+            open_trades.append(trade)
 
-            for tr in trade_reader:
-                buying = tr["SIDE"] == "B"
-                trade = Trade(tr["TIME"], tr["SYMBOL"], buying, 
-                        float(tr["PRICE"]), int(tr["QUANTITY"]))
-                
-                self.process_trade(trade)
+    def process_csv(self, file_name):
+        """Process CSV file with trades.
+
+        Parameters
+        ----------
+        file_name : string
+        """
+        trades = pd.read_csv(
+            file_name, delimiter=",", index_col="TIME", parse_dates=True, dayfirst=True
+        )
+
+        trades = trades.sort_index(ascending=True)
+
+        # Loop through each trade
+        for time, tr in trades.iterrows():
+            buying = tr["SIDE"] == "B"
+            trade = Trade(
+                time,
+                tr["SYMBOL"],
+                buying,
+                float(tr["PRICE"]),
+                int(tr["QUANTITY"]),
+            )
+
+            self.process_trade(trade)
 
     def print_closed_trades(self):
-        for ct in self._closed_trades:
+        for ct in self.closed_trades:
             print(ct)
 
     def get_pnl(self):
         return self._pnl
 
     def get_copy_of_closed_trades(self):
-        ''' returns shallow copy of closed trades '''
-        return self._closed_trades[:]
+        """returns shallow copy of closed trades"""
+        return self.closed_trades[:]
 
-class Trade():
+
+class Trade:
     def __init__(self, time, symbol, buying, price, quantity):
         self.time = time
         self.symbol = symbol
@@ -92,9 +127,20 @@ class Trade():
         self.price = price
         self.quantity = quantity
 
-class ClosedTrade():
-    def __init__(self, open_t, close_t, symbol, quantity, pnl, bought_first,
-            open_p, close_p):
+    def __str__(self):
+
+        s = str(vars(self))
+
+        return s
+
+    def __repr__(self) -> str:
+        return self.__str__()
+
+
+class ClosedTrade:
+    def __init__(
+        self, open_t, close_t, symbol, quantity, pnl, bought_first, open_p, close_p
+    ):
         self.open_t = open_t
         self.close_t = close_t
         self.symbol = symbol
@@ -107,22 +153,35 @@ class ClosedTrade():
     def __str__(self):
         s = "{},{},{},{},{:.2f},{},{},{:.2f},{:.2f}"
         s = s.format(
-                self.open_t,
-                self.close_t,
-                self.symbol,
-                self.quantity,
-                self.pnl,
-                "B" if self.bought_first else "S",
-                "S" if self.bought_first else "B",
-                self.open_p,
-                self.close_p
-                )
-        
+            self.open_t,
+            self.close_t,
+            self.symbol,
+            self.quantity,
+            self.pnl,
+            "B" if self.bought_first else "S",
+            "S" if self.bought_first else "B",
+            self.open_p,
+            self.close_p,
+        )
+
         return s
-        
+
+    def __repr__(self) -> str:
+        return self.__str__()
+
+
 if __name__ == "__main__":
-    tm = TradeManager(store_trades=False, print_trades=True)
-    print(("OPEN_TIME,CLOSE_TIME,SYMBOL,QUANTITY,PNL,"
-            "OPEN_SIDE,CLOSE_SIDE,OPEN_PRICE,CLOSE_PRICE"))
-    tm.process_csv(sys.argv[1])
-    print("{:.2f}".format(tm.get_pnl()))
+
+    tm = TradeManager()
+
+    HEADER_CLOSED = "OPEN_TIME,CLOSE_TIME,SYMBOL,QUANTITY,PNL,OPEN_SIDE,CLOSE_SIDE,OPEN_PRICE,CLOSE_PRICE".split(
+        ","
+    )
+
+    tm.process_csv("pnl_transactions_etfs.csv")
+
+    closed = [str(trade).split(",") for trade in tm.closed_trades]
+
+    print(tabulate(closed, headers=HEADER_CLOSED))
+
+    print("\nPnL = {:.2f}".format(tm.get_pnl()))
